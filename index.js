2b99366f2c7e7ef776508d54a4071115c9d7d756
Garland <garland@Garlands-MacBook-Air.local>
2017-08-17 00:28:06 -0400

    added css to button. working on logic index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+    console.log("DOM fully loaded and parsed");
+    const start = document.getElementById('start'); 
+    const stop = document.getElementById('stop');
+    const reset = document.getElementById('reset');
+    start.onclick = testChange
+
+    let timer = () => {
+    }
+    
+  });
+
