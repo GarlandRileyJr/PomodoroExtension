2c58edf59c72d2b6280ed58ba9edb0e1ac0efe2a
Garland Riley Jr <Flex-Luthor@users.noreply.github.com>
2017-08-23 19:07:18 -0400

    Timer logic (#6)
    * pushing
    
    * start button now changes clockFace element.
    
    * changed logic again
    
    * counter works in chrome popup
    
    * start, stop, and reset functions have basic functionality. need to be refactored for counting down

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 
 document.addEventListener("DOMContentLoaded", function(e) {
-    console.log("DOM fully loaded and parsed");
-    const clockFace = document.getElementById('clockFace')
-    const start = document.getElementById('start'); 
-    const stop = document.getElementById('stop');
-    const reset = document.getElementById('reset');
+  const clockFace = document.getElementById('clockFace')
+  const start = document.getElementById('start'); 
+  const stop = document.getElementById('stop');
+  const reset = document.getElementById('reset');
 
-    let endTime = new Date().getTime() + 1500000;
+  let startTime = 1500000;
 
-    let x = 0;
+  let currentTimeMin = (time) => {
+    return Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  }
 
-    let timer;
-    let timerRunning = false;
-    start.addEventListener('click', () => {
-      timerRunning = true
-      timer = setInterval(() => {
-        // call function that changes the innerHTML of the clockFace element every millisecond counting down from 25m 00s
-        clockFace.innerHTML = x
-        return x++
-      }, 1000);
-    })
+  let currentTimeSec = (time) => {
+    return Math.floor((time % (1000 * 60)) / 1000);
+  }
 
-    stop.addEventListener('click', ()=>{
-      timerRunning = false;
-      clearInterval(timer)
-    })
+  let timer;
+  let timerRunning = false;
 
-    reset.addEventListener('click', () => {
-      if(x !== 0 && !timerRunning) {
-        x = 0;
-        clockFace.innerHTML = x;
-      }
-    })
-    
+  start.addEventListener('click', () => {
+    timerRunning = true
+    timer = setInterval(() => {
+      // call function that changes the innerHTML of the clockFace element every millisecond counting down from 25m 00s
+      if (startTime === 1500000) {startTime = startTime - 1000}
+      clockFace.innerHTML = currentTimeMin(startTime) + "m " + currentTimeSec(startTime) + "s"
+      return startTime = startTime - 1000
+    }, 1000);
+  })
+
+  stop.addEventListener('click', () => {
+    timerRunning = false;
+    clearInterval(timer)
+  })
+
+  reset.addEventListener('click', () => {
+    if(startTime !== 1500000 && !timerRunning) {
+      startTime = 1500000;
+      clockFace.innerHTML = currentTimeMin(startTime) + "m " + currentTimeSec(startTime) + "0s";
+    }
+  })
 });
 
 
@@ -63,4 +69,3 @@ document.addEventListener("DOMContentLoaded", function(e) {
 //     document.getElementById("clockFace").innerHTML = "EXPIRED";
 //   }
 // }, 1000);
-
