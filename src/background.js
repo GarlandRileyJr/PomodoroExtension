c4c76407e01fa4dd868474275ae6825f33935dfc
Garland Riley Jr <Flex-Luthor@users.noreply.github.com>
2017-08-27 00:21:45 -0400

    Timer logic (#13)
    * pushing
    
    * start button now changes clockFace element.
    
    * changed logic again
    
    * counter works in chrome popup
    
    * start, stop, and reset functions have basic functionality. need to be refactored for counting down
    
    * cleaning up file
    
    * made some changes to manifest.json and format of timer display
    
    * change project architecture and renamed some files
    
    * added script.js file
    
    * code clean up in background.js
    
    * Delete .DS_Store

--- src/background.js
+++ src/background.js
@@ -1,43 +1,43 @@
 
 document.addEventListener("DOMContentLoaded", function(e) {
-  const clockFace = document.getElementById('clockFace')
-  const start = document.getElementById('start'); 
-  const stop = document.getElementById('stop');
-  const reset = document.getElementById('reset');
+  const clockFace = document.getElementById('clockFace'),
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    reset = document.getElementById('reset');
 
   let startTime = 1500000;
 
   let currentTimeMin = (time) => {
     return Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-  }
+  };
 
   let currentTimeSec = (time) => {
     return Math.floor((time % (1000 * 60)) / 1000);
-  }
+  };
 
   let timer;
   let timerRunning = false;
 
   start.addEventListener('click', () => {
-    timerRunning = true
+    timerRunning = true;
     timer = setInterval(() => {
       // call function that changes the innerHTML of the clockFace element every millisecond counting down from 25m 00s
       if (startTime === 1500000) {startTime = startTime - 1000}
-      clockFace.innerHTML = currentTimeMin(startTime) + ":" + currentTimeSec(startTime)
-      return startTime = startTime - 1000
+      clockFace.innerHTML = currentTimeMin(startTime) + ":" + currentTimeSec(startTime);
+      return startTime = startTime - 1000;
     }, 1000);
-  })
+  });
 
   stop.addEventListener('click', () => {
     timerRunning = false;
-    clearInterval(timer)
-  })
+    clearInterval(timer);
+  });
 
   reset.addEventListener('click', () => {
     if(startTime !== 1500000 && !timerRunning) {
       startTime = 1500000;
       clockFace.innerHTML = currentTimeMin(startTime) + ":" + currentTimeSec(startTime) + "0";
     }
-  })
+  });
   
 });
