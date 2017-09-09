22676028bd992dcef36ad2773fdeb3a8758a7ad8
Garland Riley Jr <Flex-Luthor@users.noreply.github.com>
2017-09-08 20:02:52 -0400

    Timer logic (#17)
    * pushing
    
    * start button now changes clockFace element.
    
    * changed logic again
    
    * counter works in chrome popup
    
    * start, stop, and reset functions have basic functionality. need to be refactored for counting down
    
    * cleaning up file
    
    * made some changes to manifest.json and format of timer display
    
    * change project architecture and renamed some files
    
    * added script.js file
    
    * code clean up in background.js
    
    * Delete .DS_Store
    
    * working on making timer code DRYer and making "break mode" functionality
    
    * fixed the format of the timer for under 60s and under 10s
    
    * added notes and mp3 sound for timer alerts
    
    * added notes to script.js

--- src/script.js
+++ src/script.js
@@ -1,2 +1,9 @@
 // This file is for testing background scripts. if this works we will be able
 // to know how persist the clock timer functions. the logic may need to be reworked.
+
+
+// make a chrome alarm that alerts 1min after you press the start button
+// next make sure that if you hit the stop button it logs how long you had
+// to go before the alarm goes off. when you hit start it'll take that time
+// as the amount of time before the alert. the reset button should clear all
+// alarms and allow you to hit start again as a new alarm cycle.
